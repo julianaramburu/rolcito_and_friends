7e5b6b25822508215194df7c76519b5fd81b7055
Julian EMAR <julianaramburu@gmail.com>
2023-10-23 17:57:06 -0300

    test

--- dndapp.py
+++ dndapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -15,3 +16,6 @@ def about():
 @app.route('/auth/login')
 def login():
     return render_template('auth/login.html')
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8000)
